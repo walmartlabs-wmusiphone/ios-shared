5488a0276d0e031f7cebd12d9978bcbac953bfc5
Doug Pedley <dpedley@gmail.com>
2015-09-19 16:24:11 -0700

    Need to pass and send Arrays as top level JSON object as well

--- Source/WebServices/SDWebService.m
+++ Source/WebServices/SDWebService.m
@@ -546,7 +546,7 @@ NSString *const SDWebServiceError = @"SDWebServiceError";
             else if ([post isKindOfClass:[NSString class]])
                 // It's a kind of NSString
                 postData = [post dataUsingEncoding:NSUTF8StringEncoding];
-            else if ([post isKindOfClass:[NSDictionary class]]) {
+            else if ([post isKindOfClass:[NSDictionary class]] || [post isKindOfClass:[NSArray class]]) {
                 // It's a kind of NSDictionary
                 NSError *jsonSerializationError = nil;
                 NSData *jsonData = [NSJSONSerialization dataWithJSONObject:post
