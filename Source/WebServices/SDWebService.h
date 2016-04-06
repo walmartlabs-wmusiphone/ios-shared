0d1762f07bf21ad8d41fcb5d8a3235ab54997ade
Angelo Di Paolo <angelod101@gmail.com>
2016-04-06 14:40:13 -0400

    remove conflict marker

--- Source/WebServices/SDWebService.h
+++ Source/WebServices/SDWebService.h
@@ -9,11 +9,9 @@
 #import "SDURLConnection.h"
 #import "Reachability.h"
 #import "SDWebServiceMockResponseProvider.h"
-<<<<<<< HEAD
 #import "SDWebServiceTask.h"
 #import "SDWebServiceEventFirehose.h"
-=======
->>>>>>> master
+
 
 typedef id (^SDWebServiceDataCompletionBlock)(NSURLResponse *response, NSInteger responseCode, NSData *responseData, NSError *error);
 typedef void (^SDWebServiceUICompletionBlock)(id dataObject, NSError *error);
